1d9cb4dae73c757512a1338c1d7cc8628dcdfae0
Liza Zhikhareva <lisalisalisa57@mail.ru>
2024-04-06 11:52:02 +0300

    Первая версия дз Н4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,31 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
+
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.marks) {
+    this.marks.push(...marks);
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+  if (this.marks) {
+    return this.marks.reduce((acc, item) => acc + item, 0) / this.marks.length;
+  } else {
+    return 0;
+  }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+  delete this.marks;
+  delete this.subject;
 }
