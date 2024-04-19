9b99be5093bcce0c760f175be5b11c618ff7f465
Liza <lisalisalisa57@mail.ru>
2024-04-19 18:30:41 +0300

    Домашнее задание по уроку Классы (1 вариант)

---
+++ 5.classes/task.js
@@ -0,0 +1,118 @@
+class PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+        this.state = 100;
+        this.type = null;
+    }
+    fix() {
+        this.state *= 1.5;
+    }
+
+    set state(state) {
+        if (state < 0) {
+            this._state = 0;
+        } else if (state > 100) {
+            this._state = 100;
+        } else this._state = state;
+    }
+
+    get state() {
+        return this._state;
+    }
+}
+
+class Magazine extends PrintEditionItem {
+    constructor(name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.type = "magazine";
+    }
+}
+
+class Book extends PrintEditionItem {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.author = author;
+        this.type = "book";
+    }
+}
+
+class NovelBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "novel";
+    }
+}
+
+class FantasticBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "fantastic";
+    }
+}
+
+class DetectiveBook extends Book {
+    constructor(author, name, releaseDate, pagesCount) {
+        super(author, name, releaseDate, pagesCount);
+        this.type = "detective";
+    }
+}
+
+
+class Library {
+    constructor(name, books) {
+        this.name = name;
+        this.books = [];
+    }
+
+    addBook(book) {
+        if (book.state > 30) {
+            this.books.push(book);
+        }
+    }
+
+    findBookBy(type, value){  
+        return this.books.find(book => book[type] == value) || null;        
+      }
+
+    
+    giveBookByName(bookName) {
+        const book = this.findBookBy('name', bookName);
+        if (book) {
+          this.books.splice(this.books.indexOf(book), 1);
+        }
+        return book;
+      }
+}
+
+// Задание 3
+
+class Student {
+    constructor(name, marks) {
+        this.name = name;
+        this.marks = {};
+    }
+
+addMarks(subject, ...score) {
+  if (this.marks.hasOwnProperty(subject)) {
+  for (let i = 0; i < score.length; i++) {
+    if (score[i] >= 2 && score[i] <= 5){
+      this.marks[subject].push(score[i]);
+    } else {
+      console.log(`Значение ${score[i]} некорректно! Оно не будет добавлено в перечень оценок по предмету ${subject}!`);
+    }
+  }
+  } else {
+    this.marks[subject] = new Array();
+    for (let i = 0; i < score.length; i++) {
+    if (score[i] >= 2 && score[i] <= 5){
+      this.marks[subject].push(score[i]);
+    } else {
+      console.log(`Значение ${score[i]} некорректно! Оно не будет добавлено в перечень оценок по предмету ${subject}!`);
+    }
+  }
+  }
+  return this.marks;
+}
+}
